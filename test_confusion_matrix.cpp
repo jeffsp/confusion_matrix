3aa53cb8b5ff5f8401c1170b8b85b9c807b98909
Jeff Perry <jeffsp@gmail.com>
2016-03-12 21:04:31 -0600

    add more tests

--- test_confusion_matrix.cpp
+++ test_confusion_matrix.cpp
@@ -19,11 +19,61 @@ void test1 ()
     clog << cm << endl;
 }
 
+void test2 ()
+{
+    confusion_matrix cm;
+    cm.update (1, 1);
+    cm.update (0, 1);
+    cm.update (1, 0);
+    cm.update (0, 0);
+    clog << cm << endl;
+    cm.update (1, 1);
+    clog << cm << endl;
+    cm.update (0, 1);
+    clog << cm << endl;
+    cm.update (1, 0);
+    clog << cm << endl;
+    cm.update (0, 0);
+    clog << cm << endl;
+}
+
+void test3 ()
+{
+    stringstream ss;
+    ss << "1 1" << endl;
+    ss << "2 1" << endl;
+    ss << "1 2" << endl;
+    ss << "2 2" << endl;
+    conditions conditions = read_conditions (ss);
+    confusion_matrix cm = get_confusion_matrix (conditions, 1);
+    clog << cm << endl;
+}
+
+void test4 ()
+{
+    stringstream ss;
+    ss << "1 1" << endl;
+    ss << "2 1" << endl;
+    ss << "1 2" << endl;
+    // oops, missing prediction
+    ss << "2" << endl;
+    bool caught = false;
+    try { conditions conditions = read_conditions (ss); }
+    catch (const exception &e) {
+        clog << "caught exception... OK" << endl;
+        caught = true;
+    }
+    verify (caught);
+}
+
 int main (int argc, char **argv)
 {
     try
     {
         test1 ();
+        test2 ();
+        test3 ();
+        test4 ();
         clog << "Success" << endl;
 
         return 0;
